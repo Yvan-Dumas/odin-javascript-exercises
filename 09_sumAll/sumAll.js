eccc8e17ddd23844cbe1d2cd349b2fc70116a233
Yvan <yvandumas05@gmail.com>
2025-07-05 19:29:49 +0200

    Resolution of exercise 09

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function (a, b) {
+    let res = 0;
+    if ((a<0 || b<0) || (!Number.isInteger(a) || !Number.isInteger(b))) {
+        return "ERROR";
+    } else if (a < b) {
+        for (let i = a; i <= b; i++) {
+            res += i;
+        }
+    } else {
+        for (let i = b; i <= a; i++) {
+            res += i;
+        }
+    }
+    return res;
 };
 
 // Do not edit below this line
