7a6fdff01e46902a77c2ad2fc6a0bb30fd2596ca
Yvan <yvandumas05@gmail.com>
2025-07-05 19:17:43 +0200

    Resolution of exercise 08

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...element) {
+    for (let i = (array.length - 1); i >=0; i--) {
+        if (element.includes(array[i])) {
+            array.splice(i, 1);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
