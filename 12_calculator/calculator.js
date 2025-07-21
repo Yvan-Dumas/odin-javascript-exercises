0d242bee86a8e8b866f38ea3c3b09779aab0c52e
Yvan <yvandumas05@gmail.com>
2025-07-21 18:23:57 +0200

    Made the exercises 12 Calculator

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  let result = 0;
+  array.forEach(num => {
+    result += num;
+  });
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  if (array.length == 0) {
+    return 0;
+  } else {
+    let result = 1;
+    array.forEach(num => {
+      result *= num
+    })
+    return result;
+  }
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return Math.pow(a,b);
 };
 
-const factorial = function() {
-	
+const factorial = function (a) {
+  if (a == 0) {
+    return 1
+  } else {
+    return factorial(a-1)*a;
+  }
 };
 
 // Do not edit below this line
