cb1114a2f87410e7c3ecdd0a7072c1a0a540124e
Yvan <yvandumas05@gmail.com>
2025-07-22 18:02:57 +0200

    Made the exercises 13 Palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let clean = str.toLowerCase().replace(/[^a-z0-9]/g, "");
+    let rev = clean.split("").reverse().join("");
 
+    if (rev == clean) {
+        return true
+    }
+    return false
 };
 
 // Do not edit below this line
